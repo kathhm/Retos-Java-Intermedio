706b8016cee21733c73b5b6c58368c1bcc60a409
Katherine Hinojosa Molina <geofisikatt@gmail.com>
2025-06-07 20:39:15 -0600

    Delete SESION01/RetosJavaIntermedioKHM directory

--- SESION01/RetosJavaIntermedioKHM/src/OrdenMasa.java
+++
@@ -1,9 +0,0 @@
-public class OrdenMasa extends OrdenProduccion{
-
-    public OrdenMasa(String codigo, int cantidad) {
-        this.codigo = codigo;
-        this.cantidad = cantidad;
-    }
-
-
-}
